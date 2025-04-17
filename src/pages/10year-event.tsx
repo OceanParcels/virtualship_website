9e6ee7b509a9a2f64046b30a753da9cb27653a9c
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2025-04-17 16:17:20 +0200

    remove redirects

--- src/pages/10year-event.tsx
+++
@@ -1,5 +0,0 @@
-import Redirect from '@/components/redirect'
-
-export default function TenYearEventRedirect() {
-  return <Redirect to='/blog/10year-event' />
-}
--- src/pages/waddendrifters.tsx
+++
@@ -1,5 +0,0 @@
-import Redirect from '@/components/redirect'
-
-export default function WadenDriftersRedirect() {
-  return <Redirect to='/blog/waddendrifters' />
-}
