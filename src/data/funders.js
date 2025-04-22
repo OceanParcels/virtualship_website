e4c70eae9ace7941b43a86e8d995cb625302cb60
Erik van Sebille <erikvansebille@gmail.com>
2025-04-22 10:41:25 +0200

    Updating funders list

--- src/data/funders.js
+++ src/data/funders.js
@@ -1,32 +1,14 @@
 export const Funders = [
   {
-    name: 'The Dutch Research Council (NWO)',
-    logo_light: '/funders-logos/NWOlogo.png',
-    logo_dark: '/funders-logos/NWOlogo_dia.png',
-    url: 'https://www.nwo.nl/en',
-  },
-  {
-    name: 'The European Research Council under the H2020 Starting Grant TOPIOS project (grant agreement No 715386).',
-    logo_light: '/funders-logos/ERClogo.png',
-    logo_dark: '/funders-logos/ERClogo_dia.png',
-    url: 'http://topios.org/',
-  },
-  {
-    name: 'ESPRC',
-    logo_light: '/funders-logos/EPSRlogo.png',
-    logo_dark: '/funders-logos/EPSRlogo_dia.png',
-    url: 'https://www.ukri.org/councils/epsrc/',
-  },
-  {
-    name: 'BMBF via the Warmworld project',
-    logo_light: '/funders-logos/BMBFlogo.png',
-    logo_dark: '/funders-logos/BMBFlogo_dia.png',
-    url: 'https://www.warmworld.de',
-  },
-  {
     name: 'Utrecht University',
     logo_light: '/funders-logos/UUlogo.png',
     logo_dark: '/funders-logos/UUlogo_dia.png',
     url: 'https://www.uu.nl/en',
+  },
+  {
+    name: 'NIOZ',
+    logo_light: '/funders-logos/NIOZlogo.png',
+    logo_dark: '/funders-logos/NIOZlogo.png',
+    url: 'https://www.nioz.nl/en',
   },
 ]
