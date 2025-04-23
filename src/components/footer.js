256515b1ccc4ae80d68389135d1306084b5fd708
Erik van Sebille <erikvansebille@gmail.com>
2025-04-23 08:55:33 +0200

    Updating links in footer items
    And also copyright message

--- src/components/footer.js
+++ src/components/footer.js
@@ -69,7 +69,7 @@ export const Footer = () => {
             </Box>
 
             <Text fontSize={'sm'}>
-              © {new Date().getFullYear()}, Parcels core developers. Apache 2.0
+              © {new Date().getFullYear()}, Virtual Ship developers. Apache 2.0
               Licensed.
             </Text>
           </Stack>
