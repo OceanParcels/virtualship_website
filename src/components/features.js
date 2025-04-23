6972e6e642d62632f39fb00e9b3644e7bd3416f0
Erik van Sebille <erikvansebille@gmail.com>
2025-04-23 07:55:47 +0200

    First update of features

--- src/components/features.js
+++ src/components/features.js
@@ -22,9 +22,7 @@ export const Features = () => {
           Key Features & Capabilities
         </Heading>
         <Text fontSize={'lg'}>
-          Parcels can be used to track passive and active particulates such as
-          water, plankton, plastic, icebergs, and fish using data from flow
-          datasets.
+          The Virtual Ship can be used to simulate and teach the planning, execution and analysis of ocean-based fieldwork
         </Text>
 
         <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={8} my={8}>
--- src/data/features.js
+++ src/data/features.js
@@ -1,34 +1,14 @@
 export const Features = [
   {
-    title: 'Use data from any (curvilinear) grid',
-    text: 'Parcels works on model outputs including NEMO, POP, MITgcm, and more.',
+    title: 'Simulate CTD, ADCP, drifter and Argo data',
+    text: '',
   },
   {
-    title: 'Combine data on different grids',
-    text: 'Create a single Parcels FieldSet from multiple different model outputs.',
+    title: 'Works with Copernicus Marine Service data',
+    text: '',
   },
   {
-    title: 'Define and execute custom kernels',
-    text: 'Encode custom particle operations such as swimming or sinking.',
-  },
-  {
-    title: 'Advect particles in 2D and 3D',
-    text: 'Use built-in kernels for Runge-Kutta4 and Runge-Kutta45 schemes.',
-  },
-  {
-    title: 'Diffuse particles in 2D',
-    text: 'Use built-in kernels for the Milstein and Euler-Maruyama schemes.',
-  },
-  {
-    title: 'Start and stop particles during execution',
-    text: 'For example, to simulate oil spills or fish spawning.',
-  },
-  {
-    title: 'Simulate particle-particle interactions',
-    text: 'For example, to simulate aggregation or mortality.',
-  },
-  {
-    title: 'Output particle data in zarr format',
-    text: 'For highly efficient output of particle data.',
+    title: 'Integrate with the Marine Facilities Planning Portal',
+    text: 'Plan your fieldwork at the marinefacilitiesplanning.com portal to and then execute with the Virtual Ship',
   },
 ]
